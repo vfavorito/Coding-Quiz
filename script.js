41f6c324026370c5a5f25b6f4f81d3220a37f421
vfavorito <vince.favorito@gmail.com>
2020-10-21 21:54:31 -0500

    Added endScreen function and HighScores Function

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let quiz = document.querySelector("#quiz");
 let response = document.querySelector("#response");
 let seconds = 0;
 let complete = 0;
-let q = 0;
+let question = 0;
 
 let responseEl = document.createElement("p");
 response.appendChild(responseEl);
@@ -13,6 +13,23 @@ response.appendChild(responseEl);
 let scoreBtnEl = document.createElement("button");
 scoreBtnEl.textContent = "View High Scores";
 highScoreBtn.appendChild(scoreBtnEl);
+
+scoreBtnEl.addEventListener("click", function () {
+    quizElStartBtn.remove();
+    quizElHead.textContent = "High Scores";
+    quizElBody.textContent = "";
+    for (let i = 0; i < localStorage.length; i++) {
+        let scoreEl = document.createElement("div");
+        scoreEl.textContent = localStorage.getItem(i);
+        quizElBody.appendChild(scoreEl);
+    }
+    let backBtn = document.createElement("button");
+    backBtn.textContent = "Go Back";
+    response.appendChild(backBtn);
+    backBtn.addEventListener("click", function () {
+        window.location.reload();
+    })
+});
 
 // Creating and Displaying Timer
 let timerEl = document.createElement("h4");
@@ -66,9 +83,14 @@ ans2Btn.setAttribute("style", "Display: none");
 ans3Btn.setAttribute("style", "Display: none");
 ans4Btn.setAttribute("style", "Display: none");
 
-
-
-// Starts counting down time once start button is clicked and stops when seconds = 0
+let initials = document.createElement("input");
+let submitBtn = document.createElement("input");
+initials.setAttribute("type", "text");
+submitBtn.setAttribute("type", "submit");
+buttonsEl.appendChild(initials);
+buttonsEl.appendChild(submitBtn);
+initials.setAttribute("style", "display: none");
+submitBtn.setAttribute("style", "display: none");
 
 function disappear() {
     setTimeout(function () {
@@ -76,24 +98,23 @@ function disappear() {
     }, 1000)
 }
 
+// Starts counting down time once start button is clicked and stops when seconds = 0
 function startTimer() {
 
     let timerInterval = setInterval(function () {
         seconds--;
         timerEl.textContent = "Time Left: " + seconds;
 
-        if (seconds <= 0) {
+        if (seconds <= 0 || complete === 1) {
             clearInterval(timerInterval);
+            return seconds;
         }
     }, 1000);
-    if (complete === 1) {
-        return;
-    }
-
 }
+
 function question1() {
-    let q = 1
-    if (q === 2) {
+    let question = 1
+    if (question === 2) {
         return;
     }
     quizElHead.textContent = "Question 1";
@@ -115,109 +136,141 @@ function question1() {
     ans4Btn.textContent = "Answer 4";
     ans4Btn.setAttribute("style", "display: block");
     ans4Btn.setAttribute("value", "wrong");
-
 }
+
 function question2() {
-    let q = 2;
-    if (q === 3) {
+    let question = 2;
+    if (question === 3) {
         return;
     }
     quizElHead.textContent = "Question 2";
     quizElBody.textContent = "insert question 2 here";
 
     ans1Btn.textContent = "Answer 1b";
-    ans1Btn.setAttribute("style", "display: block");
     ans1Btn.setAttribute("value", "wrong");
 
     ans2Btn.textContent = "Answer 2b";
-    ans2Btn.setAttribute("style", "display: block");
     ans2Btn.setAttribute("value", "correct");
 
     ans3Btn.textContent = "Answer 3b";
-    ans3Btn.setAttribute("style", "display: block");
     ans3Btn.setAttribute("value", "wrong");
 
     ans4Btn.textContent = "Answer 4b";
-    ans4Btn.setAttribute("style", "display: block");
     ans4Btn.setAttribute("value", "wrong");
 }
+
 function question3() {
-    let q = 3
-    if (q === 4) {
+    let question = 3
+    if (question === 4) {
         return;
     }
     quizElHead.textContent = "Question 3";
     quizElBody.textContent = "insert question 3 here";
 
     ans1Btn.textContent = "Answer 1c";
-    ans1Btn.setAttribute("style", "display: block");
     ans1Btn.setAttribute("value", "wrong");
 
     ans2Btn.textContent = "Answer 2c";
-    ans2Btn.setAttribute("style", "display: block");
     ans2Btn.setAttribute("value", "wrong");
 
     ans3Btn.textContent = "Answer 3c";
-    ans3Btn.setAttribute("style", "display: block");
     ans3Btn.setAttribute("value", "correct");
 
     ans4Btn.textContent = "Answer 4c";
-    ans4Btn.setAttribute("style", "display: block");
     ans4Btn.setAttribute("value", "wrong");
 }
+
 function question4() {
-    let q = 4
-    if (q === 5) {
+    let question = 4
+    if (question === 5) {
         return;
     }
     quizElHead.textContent = "Question 4";
     quizElBody.textContent = "insert question 4 here";
 
     ans1Btn.textContent = "Answer 1d";
-    ans1Btn.setAttribute("style", "display: block");
     ans1Btn.setAttribute("value", "wrong");
 
     ans2Btn.textContent = "Answer 2d";
-    ans2Btn.setAttribute("style", "display: block");
     ans2Btn.setAttribute("value", "wrong");
 
     ans3Btn.textContent = "Answer 3d";
-    ans3Btn.setAttribute("style", "display: block");
     ans3Btn.setAttribute("value", "wrong");
 
     ans4Btn.textContent = "Answer 4d";
-    ans4Btn.setAttribute("style", "display: block");
     ans4Btn.setAttribute("value", "correct");
 }
 
 function question5() {
-    let q = 5
-    if (q === 6) {
+    let question = 5
+    if (question === 6) {
         return;
     }
     quizElHead.textContent = "Question 5";
     quizElBody.textContent = "insert question 5 here";
 
     ans1Btn.textContent = "Answer 1e";
-    ans1Btn.setAttribute("style", "display: block");
     ans1Btn.setAttribute("value", "correct");
 
     ans2Btn.textContent = "Answer 2e";
-    ans2Btn.setAttribute("style", "display: block");
     ans2Btn.setAttribute("value", "wrong");
 
     ans3Btn.textContent = "Answer 3e";
-    ans3Btn.setAttribute("style", "display: block");
     ans3Btn.setAttribute("value", "wrong");
 
     ans4Btn.textContent = "Answer 4e";
-    ans4Btn.setAttribute("style", "display: block");
     ans4Btn.setAttribute("value", "wrong");
+}
+
+function endScreen() {
+    ans1Btn.setAttribute("style", "display: none");
+    ans2Btn.setAttribute("style", "display: none");
+    ans3Btn.setAttribute("style", "display: none");
+    ans4Btn.setAttribute("style", "display: none");
+    initials.setAttribute("style", "display: block");
+    submitBtn.setAttribute("style", "display: block");
+
+    quizElHead.textContent = "Complete!";
+    quizElBody.textContent = "Enter Your Initials To Save Your Score!";
+
+
+    submitBtn.addEventListener("click", function () {
+        let name = initials.value;
+        let score = name + " - " + seconds + " seconds left";
+        localStorage.setItem(localStorage.length, score);
+        highScores();
+    })
+}
+
+function highScores() {
+    initials.setAttribute("style", "display: none");
+    submitBtn.setAttribute("style", "display: none");
+    quizElHead.textContent = "High Scores";
+    quizElBody.textContent = "";
+    for (let i = 0; i < localStorage.length; i++) {
+        let scoreEl = document.createElement("div");
+        scoreEl.textContent = localStorage.getItem(i);
+        quizElBody.appendChild(scoreEl);
+    }
+    let homeBtn = document.createElement("button");
+    homeBtn.textContent = "Try Again";
+    let clearBtn = document.createElement("button");
+    clearBtn.textContent = "Clear High Scores";
+
+    quizElBody.appendChild(homeBtn);
+    quizElBody.appendChild(clearBtn);
+
+    homeBtn.addEventListener("click", function () {
+        window.location.reload();
+    })
+    clearBtn.addEventListener("click", function () {
+        localStorage.clear();
+    })
 }
 
 function main() {
     seconds = 75;
-    q++;
+    question++;
     startTimer();
     question1();
 }
@@ -233,50 +286,52 @@ buttonsEl.addEventListener("click", function (event) {
         responseEl.textContent = "Correct Answer";
         disappear();
 
-        if (q === 1) {
-            q++;
+        if (question === 1) {
+            question++;
             question2();
         }
-        else if (q === 2) {
-            q++;
+        else if (question === 2) {
+            question++;
             question3();
         }
-        else if (q === 3) {
-            q++;
+        else if (question === 3) {
+            question++;
             question4();
         }
-        else if (q === 4) {
-            q++;
+        else if (question === 4) {
+            question++;
             question5();
         }
-        else if (q === 5) {
-            q++
-            complete++
+        else if (question === 5) {
+            question++;
+            complete++;
+            endScreen();
         }
     }
-    else {
+    else if (event.target.value === "wrong") {
         responseEl.textContent = "Wrong Answer";
         seconds = seconds - 10;
         disappear();
-        if (q === 1) {
-            q++;
+        if (question === 1) {
+            question++;
             question2();
         }
-        else if (q === 2) {
-            q++;
+        else if (question === 2) {
+            question++;
             question3();
         }
-        else if (q === 3) {
-            q++;
+        else if (question === 3) {
+            question++;
             question4();
         }
-        else if (q === 4) {
-            q++;
+        else if (question === 4) {
+            question++;
             question5();
         }
-        else if (q === 5) {
-            q++
-            complete++
+        else if (question === 5) {
+            question++;
+            complete++;
+            endScreen();
         }
     }
 })
